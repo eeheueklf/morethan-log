973877aa914c744406653471531b6f6ea411a150
유경 <eeheueklf@gmail.com>
2025-03-31 15:09:30 +0900

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -56,7 +56,7 @@ const CONFIG = {
   utterances: {
     enable: true,
     config: {
-      repo: process.env.NEXT_PUBLIC_UTTERANCES_REPO || "",
+      repo: "eeheueklf/blog-comments",
       "issue-term": "og:title",
       label: "💬 Utterances",
     },
