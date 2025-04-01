64b116537eb68be27525b0436b1374c93b0c4df4
유경 <eeheueklf@gmail.com>
2025-04-01 12:46:22 +0900

    Update next-sitemap.config.js

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,7 +1,8 @@
 const { CONFIG } = require("./site.config")
 
 module.exports = {
-  siteUrl: CONFIG.link,
+  // siteUrl: CONFIG.link,
+   siteUrl: "https://hamrang.vercel.app/",
   generateRobotsTxt: true,
   sitemapSize: 7000,
   generateIndexSitemap: false,
